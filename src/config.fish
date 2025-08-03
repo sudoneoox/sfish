47234d0ce1a598943f35d63457c91b452cbb5f5a
X0Laptop@NixOS <diegoa2992@proton.me>
2025-08-03 02:29:00 -0500

    remove

--- src/config.fish
+++ src/config.fish
@@ -4,5 +4,3 @@ source $XDG_CONFIG_HOME/fish/aliases.fish
 source $XDG_CONFIG_HOME/fish/developer.fish
 source $XDG_CONFIG_HOME/fish/main.fish
 source $XDG_CONFIG_HOME/fish/prompt.fish
-
-fish_add_path /home/diego/.spicetify
--- src/developer.fish
+++ src/developer.fish
@@ -1,39 +1,37 @@
-
 #  * `ccat` Cat file, but with syntax highlighting
 alias ccat='pygmentize -g'
 
 #  * `git-show-unpushed-commits` Git show unpushed commits
-alias git-show-unpushed-commits='git cherry -v' 
+alias git-show-unpushed-commits='git cherry -v'
 
 function git-revert-file --description "Revert single file in git"
-  git reset HEAD $argv; git checkout $argv
+    git reset HEAD $argv
+    git checkout $argv
 end
 
 function git-wat --description "Show 5 latest commits"
-  git log --pretty="format:%Cgreen%<(17,trunc)%an%Creset%s" --abbrev-commit  HEAD~5..HEAD
+    git log --pretty="format:%Cgreen%<(17,trunc)%an%Creset%s" --abbrev-commit HEAD~5..HEAD
 end
 
 function git-recent-branches --description "Show recentbranches"
-  git branch --sort=-committerdate
+    git branch --sort=-committerdate
 end
 
 function git-pr --description "Create PR on Github"
-  set from   (git config --get remote.origin.url | string match --regex "\:(.*)/" | tail -1)
-  set to     (git config --get remote.oss.url | string match --regex "\:(.*)\.git" | tail -1)
-  set branch (git rev-parse --abbrev-ref HEAD)
-  
-  xdg-open "https://github.com/$to/compare/master...$from:$branch?expand=1"
-end
+    set from (git config --get remote.origin.url | string match --regex "\:(.*)/" | tail -1)
+    set to (git config --get remote.oss.url | string match --regex "\:(.*)\.git" | tail -1)
+    set branch (git rev-parse --abbrev-ref HEAD)
 
+    xdg-open "https://github.com/$to/compare/master...$from:$branch?expand=1"
+end
 
 function td --description "Add to ~/todo.md list"
-  echo -e "\n- [ ] $argv" >> ~/todo.md
+    echo -e "\n- [ ] $argv" >>~/todo.md
 end
-
 
 #  * `server-here` Start serving files in current directory and open browser
 alias server-here='python3 -m http.server 8888 && xdg-open http://localhost:8888'
 
 function bak --description "Copies (backups) file in same folder with .bak extension"
-  cp -i "$argv" "$argv.bak"
+    cp -i "$argv" "$argv.bak"
 end
