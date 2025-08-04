4c7ec4e86105d3b9d44c6dd74775e16a9866f977
X0Laptop@NixOS <diegoa2992@proton.me>
2025-08-03 19:32:55 -0500

    starship

--- src/config.fish
+++ src/config.fish
@@ -1,6 +1,7 @@
 set -q XDG_CONFIG_HOME || set XDG_CONFIG_HOME "$HOME/.config"
+starship init fish | source
 
 source $XDG_CONFIG_HOME/fish/aliases.fish
 source $XDG_CONFIG_HOME/fish/developer.fish
 source $XDG_CONFIG_HOME/fish/main.fish
-source $XDG_CONFIG_HOME/fish/prompt.fish
+# source $XDG_CONFIG_HOME/fish/prompt.fish
